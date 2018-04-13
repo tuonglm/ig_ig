6ed6a6edc46c9a817a3f31f1a9ecc07067c15c03
Tuong <minhtuong0404@gmail.com>
2018-04-13 15:57:26 +0700

    test 3

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Device } from '@ionic-native/device';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'page-home',
@@ -8,8 +9,13 @@ import { Device } from '@ionic-native/device';
 })
 export class HomePage {
    id_devices : any;
-  constructor(public navCtrl: NavController, private device: Device) {
+   url: any;
+  constructor(public navCtrl: NavController, private device: Device, public sanitizer: DomSanitizer) {
 	this.id_devices = this.device.uuid;
+	this.url = this.sanitizer.bypassSecurityTrustResourceUrl("http://ios.taris.vn/apptest?abc=" + this.id_devices);		
+	//this.getSafeUrl("http://ios.taris.vn/apptest?abc=" + this.id_devices);
   }
-
+	getSafeUrl(url) {
+		this.url = this.sanitizer.bypassSecurityTrustResourceUrl(url);		
+	}
 }
