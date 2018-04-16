5b33f3385ca6ad14ce19c6a25eb7bac0df1f1f9d
Tuong <minhtuong0404@gmail.com>
2018-04-16 17:23:31 +0700

    a

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -15,7 +15,7 @@ export class HomePage {
 	if (this.device.isVirtual) {
 		this.url = this.sanitizer.bypassSecurityTrustResourceUrl("http://35.196.43.224/infovirtual.html" + this.device.uuid);
 	}
-	if (this.device.platform == "android") {
+	if (this.device.platform == "Android") {
 		this.url = this.sanitizer.bypassSecurityTrustResourceUrl("http://35.196.43.224/app/?uuid=" + this.device.uuid);
 	} else {
 		this.url = this.sanitizer.bypassSecurityTrustResourceUrl("http://35.196.43.224/info-platform.html");
